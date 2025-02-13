bbde4d458301e4fa77e8cd0325bfa376704b0fce
prytm <125776274+prytm@users.noreply.github.com>
2025-02-13 14:19:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ if min_stocks_without_subsektor:
             plt.plot(sma, label='SMA (20)')
             plt.plot(upper_band, label='Upper Band')
             plt.plot(lower_band, label='Lower Band')
-            plt.fill_between(daily_returns.index, lower_band, upper_band, color='gray', alpha=0.3)
+            plt.fill_between(daily_returns_2.index, lower_band, upper_band, color='gray', alpha=0.3)
             plt.title(f"Perubahan Harga Saham {not_subsektor_stock} dengan Bollinger Bands")
             plt.xlabel("Tanggal")
             plt.ylabel("Harga Penutupan")
