d3c05430fd5cef64e25add669aabdfc036713765
prytm <125776274+prytm@users.noreply.github.com>
2025-03-14 10:20:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,13 +16,15 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded")
 
+hide_github_icon = “”"
+
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+“”"
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 # Custom CSS to inject into Streamlit
 st.markdown("""
 <style>
-
-#MainMenu {
-  visibility: hidden;
-}
 
 /* Adjust the size and alignment of the CALL and PUT value containers */
 .metric-container {
